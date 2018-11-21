1ee3eb8d22c80f2dc47cbe679d08e66f51879e83
MarkusKurzmann <20150081@HTBLAPerg.onmicrosoft.com>
2018-11-21 21:33:40 +0100

    Implemented Homework

---
+++ app.ts
@@ -0,0 +1,62 @@
+import { Dictionary } from "lodash";
+
+let ar = new Array();
+let lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('openthesaurus.txt')
+});
+
+//read the file line by line
+lineReader.on('line', function (line) {
+    ar.push(line);
+});
+
+lineReader.on('close', function (line) {
+    //check if there were any words passed to the program
+    if(process.argv.length < 3){
+        console.log("Please choose some words for the thesaurus...");
+    }else{
+
+        let words: string[] = new Array();
+        let synonyms : Dictionary<string> = {
+            "key": "value"        
+        }
+        
+        for(var i = 2; i < process.argv.length; i++){
+            words[i-2] = process.argv[i];
+            synonyms[words[i-2]] = words[i-2];
+        }
+
+        for(var i = 0; i < ar.length; i++){
+            let line: string[] = ar[i].split(";");
+            
+            //found valid line
+            if(line.length > 1){
+
+                //check for every word you pass to your program
+                for(var w = 0; w < words.length; w++){
+
+                    //inspect every word in a valid line if its the same like the word you are searching
+                    for(var j = 0; j < line.length; j++){
+
+                        //check if word appears in line
+                        if(line[j] === words[w]){
+
+                            //word appears in line -> save rest of line
+                            for(var k = 1; k < line.length; k++){
+                                synonyms[words[w]] += ", " + line[k];
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        for(var i = 0; i < words.length; i++){
+            console.log("Found synonyms for '",words[i], "':\n");
+            let wordsSplit: string[] = synonyms[words[i]].split(",");
+            for(var j = 0; j < wordsSplit.length; j++){
+                console.log("\t",wordsSplit[j]);
+            }
+        }
+    }
+});
